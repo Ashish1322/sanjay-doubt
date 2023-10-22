199e15a01cbe59a47eae648396bdad95ac71b64f
ashish4323 <ashish.k@fwc.co.in>
2023-10-22 18:51:06 +0530

    Fixed the issues

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,43 @@
+import  { useState } from 'react';
+import { useContext } from 'react';
+import TodoContext from '../TodoContext';
+import{Link} from "react-router-dom"
+export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const {login} = useContext(TodoContext);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        login(email,  password);
+    };
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+        <h1>Login Now</h1>
+        <input 
+        onChange={(e)=> setEmail (e.currentTarget.value)}
+         type="email"
+         placeholder="Enter Your Email" 
+         />
+        <br/>
+        <input 
+        onChange={(e)=> setPassword (e.currentTarget.value)} 
+        type="Password"
+        placeholder="Enter Your Password" 
+        />
+        <br/>
+        
+        <input type="submit" value="Login"/><br/>
+        <Link to="/forget-password"> Forget Password?</Link>
+        <br/>
+        <Link to="/signup"> Dont have account, Register?</Link>
+        
+        </form>
+    </div>
+  );
+}
---
+++ authentication/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const cors = require("cors");
+
+//import routers
+
+const authRouter = require("./routes/auth");
+const todoRouter = require("./routes/todo");
+
+//setting app
+app.use(express.json());
+app.use(cors());
+
+//configuring Rounters
+app.use("/auth", authRouter);
+app.use("/todo", todoRouter);
+
+//Database connection
+mongoose
+  .connect(
+    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.6"
+  )
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log("Error connecting", err.message));
+
+app.listen(3010, () => console.log("Server is running at port 3010"));
